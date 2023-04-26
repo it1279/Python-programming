5b4e36421ba0af30814fe8c4e7dd5007c9d7dc47
it1279 <120377606+it1279@users.noreply.github.com>
2023-04-26 15:49:19 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,8 @@
+import math
+x1=int(input("enter x1:"))
+y1=int(input("enter y1:"))
+x2=int(input("enter x2:"))
+y2=int(input("enter y2:"))
+distance=math.sqrt(((x2-x1)**2)+((y2-y1)**2))
+print("the distance between the two points is:",distance)
+
